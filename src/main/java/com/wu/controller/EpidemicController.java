79a3aba7722177c96bded1213367ebc3c21a2e7f
TOM <toms163wyemail@163.com>
2023-07-04 18:25:12 +0800

    EmpidenController1

--- src/main/java/com/wu/controller/EpidemicController.java
+++ src/main/java/com/wu/controller/EpidemicController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/empiden")
-public class EmpIdenController {
+public class EpidemicController {
 
     @Autowired
     EpidemicService epidemicService;
